23885198de825947315b752f7e6280ed3e7ad036
Akande Oluwaseun <akandeseun44@gmail.com>
2022-02-11 00:28:12 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ADC3D2',
+        secondary: '#393C41',
+        tertiary: '#ECECEB',
+      },
+      fontFamily: {
+        body: ['Montserrat']
+      }
+    },
+  },
+  plugins: [],
+}
